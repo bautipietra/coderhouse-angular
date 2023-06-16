508a8a3bd0f3d61e1ff3cf431dc2036b83f0f881
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-06-15 22:16:03 -0300

    ⭐ Primer desafío

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContentComponent } from './components/content/content.component';
+import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AsideComponent,
+    ContentComponent,
+    MainWrapperComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
