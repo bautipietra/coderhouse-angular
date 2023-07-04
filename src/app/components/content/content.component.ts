049f6e571f44c98059ee3e6326f19066263c6bca
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-07-04 19:01:18 -0300

    🚀 Entrega interpolación y directivas

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  alumnos: string[] = ['Ejemplo Correcto', "Ejemplo 1nc0rr3ct0"];
+  alumnoNuevo: string = '';
+  
+  onInputChange(event: KeyboardEvent): void {
+    const htmlInput = event.target as HTMLInputElement;
+    this.alumnoNuevo = htmlInput?.value
+  }
 
+  onSave(): void {
+    this.alumnos.push(this.alumnoNuevo);
+  }
 }
